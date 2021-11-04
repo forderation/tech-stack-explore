f45bfb222cc89b7172eee4bec4e21ef963d3053a
Kharisma Muzaki <kharisma.muzaki@gmail.com>
2021-11-04 20:31:32 +0700

    add grpc service

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -1,18 +1,106 @@
-package client
+package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
 
+	"github.com/forderation/tech-stack-explore/grpc/common/config"
 	"github.com/forderation/tech-stack-explore/grpc/common/model"
-	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 )
 
-type UsersClient interface {
-	Register(ctx context.Context, in *model.User, opts ...grpc.CallOption) (*empty.Empty, error)
-	List(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*model.UserList, error)
+func serviceGarage() model.GaragesClient {
+	port := config.SERVICE_GARAGE_PORT
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect to", port, err)
+	}
+	return model.NewGaragesClient(conn)
+}
+
+func serviceUser() model.UsersClient {
+	port := config.SERVICE_USER_PORT
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect to", port, err)
+	}
+	return model.NewUsersClient(conn)
 }
 
 func main() {
+	user1 := model.User{
+		Id:       "n001",
+		Name:     "zaaki zaki",
+		Password: "hooman try to b3st",
+		Gender:   model.UserGender(model.UserGender_value["MALE"]),
+	}
 
+	user2 := model.User{
+		Id:       "n001",
+		Name:     "trifebsi",
+		Password: "let the w0rld run",
+		Gender:   model.UserGender(model.UserGender_value["FEMALE"]),
+	}
+
+	userService := serviceUser()
+	fmt.Println("\n", "==========> user test")
+
+	// Register user 1
+	userService.Register(context.Background(), &user1)
+	userService.Register(context.Background(), &user2)
+
+	garageService := serviceGarage()
+	fmt.Println("\n", "======> Garage Test")
+	garage1 := model.Garage{
+		Id:   "q001",
+		Name: "Quel'thalas",
+		Coordinate: &model.GarageCoordinate{
+			Latitude:  45.123123123,
+			Longitude: 54.1231313123,
+		},
+	}
+	garage2 := model.Garage{
+		Id:   "q002",
+		Name: "Jakarta",
+		Coordinate: &model.GarageCoordinate{
+			Latitude:  45.123123123,
+			Longitude: 54.1231313123,
+		},
+	}
+
+	// Add both garage 1 to user1
+	garageService.Add(context.Background(), &model.GarageAndUserId{
+		UserId: user1.Id,
+		Garage: &garage1,
+	})
+
+	// Show all garages user 1
+	garagesUser1, err := garageService.List(context.Background(), &model.GarageUserId{
+		UserId: user1.Id,
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	garagesUser1Bytes, _ := json.Marshal(garagesUser1.List)
+	log.Println("user1 garages: ", string(garagesUser1Bytes))
+
+	// Add garage 2 only to user2
+	garageService.Add(context.Background(), &model.GarageAndUserId{
+		UserId: user2.Id,
+		Garage: &garage2,
+	})
+
+	// Show all garages user 2
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	garagesUser2, err := garageService.List(ctx, &model.GarageUserId{
+		UserId: user2.Id,
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	garageUser2Bytes, _ := json.Marshal(garagesUser2.List)
+	log.Println("user2 garages:", string(garageUser2Bytes))
 }
--- grpc/services/service-garage/main.go
+++ grpc/services/service-garage/main.go
@@ -1,5 +1,51 @@
-package servicegarage
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/forderation/tech-stack-explore/grpc/common/config"
+	"github.com/forderation/tech-stack-explore/grpc/common/model"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+)
+
+var localStorage *model.GarageListByUser
+
+func init() {
+	localStorage = new(model.GarageListByUser)
+	localStorage.List = make(map[string]*model.GarageList)
+}
+
+type GaragesServer struct{}
+
+func (GaragesServer) Add(ctx context.Context, param *model.GarageAndUserId) (*empty.Empty, error) {
+	userId := param.UserId
+	garage := param.Garage
+
+	if _, ok := localStorage.List[userId]; !ok {
+		localStorage.List[userId] = new(model.GarageList)
+		localStorage.List[userId].List = make([]*model.Garage, 0)
+	}
+	localStorage.List[userId].List = append(localStorage.List[userId].List, garage)
+	log.Println("adding garage", garage.String(), "for user", userId)
+	return new(empty.Empty), nil
+}
+
+func (GaragesServer) List(ctx context.Context, param *model.GarageUserId) (*model.GarageList, error) {
+	userId := param.UserId
+	return localStorage.List[userId], nil
+}
 
 func main() {
-
+	grpcServer := grpc.NewServer()
+	var garageServer GaragesServer
+	model.RegisterGaragesServer(grpcServer, garageServer)
+	log.Println("Starting RPC server at", config.SERVICE_GARAGE_PORT)
+	listener, err := net.Listen("tcp", config.SERVICE_GARAGE_PORT)
+	if err != nil {
+		log.Fatalf("could not listen to %s: %v", config.SERVICE_GARAGE_PORT, err)
+	}
+	log.Fatal(grpcServer.Serve(listener))
 }
